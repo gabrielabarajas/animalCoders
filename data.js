a5cb0e171feb97e9a6f70755325acdcb988c98c6
unknown <gabybarajasbcn@gmail.com>
2022-11-22 14:45:48 +0100

    estructura inicial de html

---
+++ data.js
@@ -0,0 +1,28 @@
+productos = [
+    {
+        image: "https://static.miscota.com/media/1/photos/products/107596/UNSCENTBIO300-USA-HORIZONTAL-BAG-2-736x736-636375798cda1-g-637b48aa393c4_g.webp",
+        title: "Bolsita",
+        price: 2.99,
+    },
+    {
+        image: "",
+        title: "",
+        price: "",
+    },
+    {
+        image: "",
+        title: "",
+        price: "",
+    },
+    {
+        image: "",
+        title: "",
+        price: "",
+    },    
+    {
+        image: "",
+        title: "",
+        price: "",
+    },
+
+]
