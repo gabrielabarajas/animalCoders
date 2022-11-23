5062032979f80906c62206e75a91ee982f54d0d8
unknown <gabybarajasbcn@gmail.com>
2022-11-23 08:22:31 +0100

    data inicial

--- data.js
+++ data.js
@@ -1,45 +1,54 @@
 productos = [
     {
+        id: 0,
         image: "https://static.miscota.com/media/1/photos/products/129671/PSJ1AE-4-20190513-20190513214005-20190513214406-1000x1000-6284fee971101_g.jpg",
         title: "KONG Juguete Mordedor Squeez Jels para Perros",
         price: 5.49,
     },
     {
+        id: 1,
         image: "https://static.miscota.com/media/1/photos/products/503053/OIeP-wZY-630f18d38916a_g.jpg",
         title: "FuzzYard Sudadera Turtle Teddy Rosa para Perro",
         price: 21.24,
     },
     {
+        id: 2,
         image: "https://static.miscota.com/media/1/photos/products/109518/protector-solar-para-gatos-60ml-62a73d651f4f1_g.png",
         title: "Men For San Protector Solar 30+ para Perros y Gatos",
         price: 11.16,
     },
     {
+        id: 3,
         image: "https://static.miscota.com/media/1/photos/products/114675/114675-lubrithal_1_g.jpg",
         title: "Dechra Crema Hidratante Ocular Lubrithal",
         price: 16.95,
     },    
     {
+        id: 4,
         image: "https://static.miscota.com/media/1/photos/products/042657/88-63627821e9590_g.png",
         title: "Inodorina Spray Desodorante de Talco para Perros y Gatos",
         price: 6.15,
     },    
     {
+        id: 5,
         image: "https://static.miscota.com/media/1/photos/products/070368/70368-75653-75653-as00237-1-1-1_1_g.jpg",
         title: "Chien Chic Perfume Melocoton - Spray",
         price: 12.79,
     },
     {
+        id: 6,
         image: "https://static.miscota.com/media/1/photos/products/049252/49252-24802_0_g.jpeg",
         title: "Dermoscent Bálsamo Bio Balm Reparador para Almohadillas y Trufa",
         price: 17.95,
     },
     {
+        id: 7,
         image: "https://static.miscota.com/media/1/photos/products/049144/49144-adofilm_0_g.jpg",
         title: "Calier Pomada Ado Film Protectora y Cicatrizante",
         price: 11.95,
     },
     {
+        id: 8,
         image: "https://static.miscota.com/media/1/photos/products/069333/69333-5414365062705_1_g.jpg",
         title: "Duvo Plus Albornoz Perro Microfibra Azul",
         price: 20.92,
